e9a25cdef1485aedc99fd1325b42a9e041327ecd
DiegoJ <diegojimenezescutia077@gmail.com>
2025-11-13 12:46:43 -0600

    Update Eliminacion_B_arboles.py

--- Eliminacion_B_arboles.py
+++ Eliminacion_B_arboles.py
@@ -1,5 +1,5 @@
-Implementa aquí todos los procesos necesarios para la operación de eliminación. 
-Pueden modificar la extensión del documento para que se ajuste al lenguaje de su elección y comentar estas instrucciones.
+#Implementa aquí todos los procesos necesarios para la operación de eliminación. 
+#Pueden modificar la extensión del documento para que se ajuste al lenguaje de su elección y comentar estas instrucciones.
 
 class NodoB:
     def __init__(self, t, hoja):
